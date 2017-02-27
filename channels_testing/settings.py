bfc592c3aef9cede2f7d4d733ca0b428626edb12
Emma <emma.upton.95@gmail.com>
2017-02-27 22:11:29 +0000

    updated settings

--- channels_testing/settings.py
+++ channels_testing/settings.py
@@ -56,6 +56,7 @@ CHANNEL_LAYERS= {
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'channels_testing.urls'
